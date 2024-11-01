3104296f00332a3bc4e84f0e96779dbfb8bd7c7f
Vinicius-O-Ferraz <vinicius.f.ferraz.bsi@gmail.com>
2024-11-01 15:40:46 -0300

    minor refractor

--- binaryTrees/src/BinaryTree.java
+++ binaryTrees/src/BinaryTree.java
@@ -1,58 +1,3 @@
-//public class BinaryTree {
-//    Node root;
-//
-//    public BinaryTree() {
-//        root = null;
-//    }
-//
-//    public void insert(int key) {
-//        root = insertRec(root, key);
-//    }
-//
-//    private Node insertRec(Node root, int key) {
-//        if (root == null) {
-//            root = new Node(key);
-//            return root;
-//        }
-//
-//        if (key < root.key)
-//            root.left = insertRec(root.left, key);
-//        else if (key > root.key)
-//            root.right = insertRec(root.right, key);
-//        return root;
-//    }
-//
-//    public void inOrder() {inOrderRec(root);}
-//
-//    private void inOrderRec(Node root) {
-//        if (root != null) {
-//            inOrderRec(root.left);
-//            System.out.println(root.key + " ");
-//            inOrderRec(root.right);
-//        }
-//
-//    private boolean searchRec(Node root, int key) {
-//        if (root == null)
-//            return false;
-//
-//        if (root.key == key)
-//            return true;
-//
-//        if (key < root.key)
-//            return searchRec(root.left, key);
-//        else
-//            return searchRec(root.right, key);
-//    }
-//
-//    public boolean search(int key){
-//        return searchRec(root,key);
-//    }
-//
-//    }
-//}
-
-
-// BinaryTree Class
 public class BinaryTree {
     Node root;
 
@@ -60,35 +5,23 @@ public class BinaryTree {
         root = null;
     }
 
-    // Method to insert a new node with given key
-    public void insert(int key) {
-        root = insertRec(root, key);
-    }
+    public void insert(int key) {root = insertRec(root, key);}
 
-    // A recursive function to insert a new key in BST
     private Node insertRec(Node root, int key) {
-        // If the tree is empty, return a new node
         if (root == null) {
             root = new Node(key);
             return root;
         }
 
-        // Otherwise, recur down the tree
-        if (key < root.key)
-            root.left = insertRec(root.left, key);
-        else if (key > root.key)
-            root.right = insertRec(root.right, key);
-
-        // return the (unchanged) node pointer
+        if (key < root.key){root.left = insertRec(root.left, key);}
+        else if (key > root.key){root.right = insertRec(root.right, key);}
         return root;
     }
 
-    // Method to print the tree inorder
     public void inorder() {
         inorderRec(root);
     }
 
-    // A utility function to do inorder traversal of BST
     private void inorderRec(Node root) {
         if (root != null) {
             inorderRec(root.left);
@@ -97,46 +30,34 @@ public class BinaryTree {
         }
     }
 
-    // Method to search for a key in the tree
-    public boolean search(int key) {
-        return searchRec(root, key);
-    }
+    public boolean search(int key) {return searchRec(root, key);}
 
-    // A utility function to search for a key in BST
     private boolean searchRec(Node root, int key) {
-        if (root == null)
-            return false;
+        if (root == null){return false;}
 
-        if (root.key == key)
-            return true;
+        if (root.key == key){return true;}
 
-        if (key < root.key)
-            return searchRec(root.left, key);
-        else
-            return searchRec(root.right, key);
+        if (key < root.key){return searchRec(root.left, key);}
+
+        else{return searchRec(root.right, key);}
     }
 
-    public int findMin() {
-        return findMinRec(root);
-    }
+    public int findMin() {return findMinRec(root);}
 
     private int findMinRec(Node root) {
         if (root == null) {throw new IllegalArgumentException("Tree is empty");}
+
         if (root.left == null) {return root.key;}
+
         return findMinRec(root.left);
     }
 
-    public int findMax() {
-        return findMaxRec(root);
-    }
+    public int findMax() {return findMaxRec(root);}
 
-    // A utility function to find the maximum value in BST
     private int findMaxRec(Node root) {
-        if (root == null)
-            throw new IllegalStateException("Tree is empty");
+        if (root == null){throw new IllegalStateException("Tree is empty");}
 
-        if (root.right == null)
-            return root.key;
+        if (root.right == null){return root.key;}
 
         return findMaxRec(root.right);
     }
--- binaryTrees/src/Main.java
+++ binaryTrees/src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BinaryTree tree = new BinaryTree();
+
+        tree.insert(80);
+        tree.insert(90);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(50);
+        tree.insert(10);
+        tree.insert(60);
+        tree.insert(70);
+        tree.insert(100);
+
+        System.out.println("In order transversal: ");
+        tree.inorder();
+
+        int searchKey = 43;
+        if (tree.search(searchKey))
+            System.out.println("\nKey " + searchKey + " found in the tree. ");
+        else
+            System.out.println("\nKey " + searchKey + " not found in the tree. ");
+
+        System.out.println("The minimum value in the tree : " +tree.findMin());
+        System.out.println("The maximum value in the tree : " +tree.findMax());
     }
 }
